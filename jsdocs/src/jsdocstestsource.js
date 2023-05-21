409abbed2bd5e508aeb02ee1a678d35c92fe1827
khushijpatel <kjp002@ucsd.edu>
2023-05-21 09:45:54 -0700

    test jsdocs

---
+++ jsdocs/src/jsdocstestsource.js
@@ -0,0 +1,13 @@
+/**
+ * Two numbers added together
+ *
+ * @param a {number}
+ * @param b {number}
+ *
+ * @example
+ * add(1, 2) returns 3
+ * 
+ */
+export const add = (a, b) => {
+    return a + b;
+};
