37bb147d1909eac01a1e1890450ebab6b44449c3
Sora <70079892+sorata000x@users.noreply.github.com>
2023-05-26 11:39:04 -0700

    Remove zooming

--- source/pages/skymap_page/skymap.js
+++ source/pages/skymap_page/skymap.js
@@ -36,7 +36,6 @@ function init() {
 function setCanvas() {
     let canvas = document.querySelector("canvas");
     let ctx = canvas.getContext('2d');
-    let cameraZoom = 1;
     let cameraOffset = { x: 0, y: 0 };
     let MAX_ZOOM = 5;
     let MIN_ZOOM = 0.1;
@@ -49,8 +48,6 @@ function setCanvas() {
     canvas.addEventListener('touchend',  (e) => handleTouch(e, onPointerUp));
     canvas.addEventListener('mousemove', onPointerMove);
     canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove));
-    // Zooming
-    canvas.addEventListener('wheel', (e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY));
 
     draw();
 
@@ -60,7 +57,6 @@ function setCanvas() {
         ctx.save();
         // Translate to the canvas centre before zooming - so you'll always zoom on what you're looking directly at
         ctx.translate( window.innerWidth/2, window.innerHeight/2 )
-        ctx.scale(cameraZoom, cameraZoom)
         ctx.translate( -window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y )
         ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
         requestAnimationFrame( draw )
@@ -81,20 +77,18 @@ function setCanvas() {
 
     function onPointerDown(e) {
         isDragging = true
-        dragStart.x = getEventLocation(e).x/cameraZoom - cameraOffset.x
-        dragStart.y = getEventLocation(e).y/cameraZoom - cameraOffset.y
+        dragStart.x = getEventLocation(e).x - cameraOffset.x
+        dragStart.y = getEventLocation(e).y - cameraOffset.y
     }
 
     function onPointerUp(e) {
         isDragging = false
-        initialPinchDistance = null
-        lastZoom = cameraZoom
     }
 
     function onPointerMove(e) {
         if (isDragging) {
-            cameraOffset.x = getEventLocation(e).x/cameraZoom - dragStart.x
-            cameraOffset.y = getEventLocation(e).y/cameraZoom - dragStart.y
+            cameraOffset.x = getEventLocation(e).x - dragStart.x
+            cameraOffset.y = getEventLocation(e).y - dragStart.y
         }
     }
 
@@ -105,43 +99,6 @@ function setCanvas() {
         else if (e.type == "touchmove" && e.touches.length == 2) {
             isDragging = false
             handlePinch(e)
-        }
-    }
-
-    // Handle zooming events
-
-    let initialPinchDistance = null
-    let lastZoom = cameraZoom
-
-    function handlePinch(e) {
-        e.preventDefault()
-        
-        let touch1 = { x: e.touches[0].clientX, y: e.touches[0].clientY }
-        let touch2 = { x: e.touches[1].clientX, y: e.touches[1].clientY }
-        
-        // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
-        let currentDistance = (touch1.x - touch2.x)**2 + (touch1.y - touch2.y)**2
-        
-        if (initialPinchDistance == null)
-            initialPinchDistance = currentDistance;
-        else
-            adjustZoom( null, currentDistance/initialPinchDistance );
-    }
-
-    function adjustZoom(zoomAmount, zoomFactor) {
-        if (!isDragging) {
-            if (zoomAmount) {
-                cameraZoom += zoomAmount
-            }
-            else if (zoomFactor) {
-                console.log(zoomFactor)
-                cameraZoom = zoomFactor*lastZoom
-            }
-            
-            cameraZoom = Math.min( cameraZoom, MAX_ZOOM )
-            cameraZoom = Math.max( cameraZoom, MIN_ZOOM )
-            
-            console.log(zoomAmount)
         }
     }
 
