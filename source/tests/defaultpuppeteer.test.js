d374e7f71dab92ace67c36750713366c7276805c
ShashankVenkatramani <cjvshashank@gmail.com>
2023-06-02 12:58:28 -0700

    Added puppeteer test for localhost attempt

--- source/tests/defaultpuppeteer.test.js
+++ source/tests/defaultpuppeteer.test.js
@@ -1,9 +1,19 @@
-describe('Google', () => {
+describe('Google Sanity Check Test Case', () => {
     beforeAll(async () => {
         await page.goto('https://google.com');
     });
   
     it('should be titled "Google"', async () => {
-        await expect(page.title()).resolves.toMatch('ASDFASDF');
+        await expect(page.title()).resolves.toMatch('Google');
+    });
+});
+
+describe('Website Load Check', () => {
+    beforeAll(async () => {
+        await page.goto('https://localhost:4444/source/pages/landing_page/landing.html');
+    });
+  
+    it('should be titled ', async () => {
+        await expect(page.title()).resolves.toMatch('Landing Page');
     });
 });
