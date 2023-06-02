23af5814f6042a076416419e5d428089e64d8af1
ShashankVenkatramani <cjvshashank@gmail.com>
2023-06-02 13:35:52 -0700

    Swapped out puppeteer npm serve for http-server

--- source/tests/jest-puppeteer.config.js
+++ source/tests/jest-puppeteer.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-    launch: {
-      dumpio: true,
-      headless: true
-    },
-    server: {
-      command: `npm start`,
-      port: 4444,
-      launchTimeout: 10000,
-      debug: true,
-    },
-}
+// module.exports = {
+//     launch: {
+//       dumpio: true,
+//       headless: true
+//     },
+//     server: {
+//       command: `npm start`,
+//       port: 4444,
+//       launchTimeout: 10000,
+//       debug: true,
+//     }
+// }
