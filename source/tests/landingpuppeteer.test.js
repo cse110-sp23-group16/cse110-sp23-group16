74b3a44e3d90ab32c8290e58cba3b08bcc1d3a0b
ShashankVenkatramani <cjvshashank@gmail.com>
2023-06-08 18:40:41 -0700

    Fix prettier

--- source/tests/landingpuppeteer.test.js
+++ source/tests/landingpuppeteer.test.js
@@ -9,23 +9,23 @@ describe("Landing Page Tests", () => {
     // Reset the page
     await page.evaluate(() => {
       localStorage.clear();
-    })
+    });
     await page.reload();
 
     // Navigate through the landing page to get to skymap
-    const startbutton = await page.$('#start-button');
+    const startbutton = await page.$("#start-button");
     await startbutton.click();
     await page.waitForTimeout(100);
-    const healthbutton = await page.$('#health-button');
+    const healthbutton = await page.$("#health-button");
     await healthbutton.click();
     await page.waitForTimeout(100);
-    const continuebutton = await page.$('#continue-button');
+    const continuebutton = await page.$("#continue-button");
     await continuebutton.click();
     await page.waitForTimeout(100);
 
     // Check local storage
     const questiontype = await page.evaluate(() => {
-      return localStorage.getItem('questionType');
+      return localStorage.getItem("questionType");
     });
 
     expect(questiontype).toBe("health");
@@ -34,23 +34,23 @@ describe("Landing Page Tests", () => {
     // Reset the page
     await page.evaluate(() => {
       localStorage.clear();
-    })
+    });
     await page.reload();
-    
+
     // Navigate through the landing page to get to skymap
-    const startbutton = await page.$('#start-button');
+    const startbutton = await page.$("#start-button");
     await startbutton.click();
     await page.waitForTimeout(100);
-    const relationshipbutton = await page.$('#relationship-button');
+    const relationshipbutton = await page.$("#relationship-button");
     await relationshipbutton.click();
     await page.waitForTimeout(100);
-    const continuebutton = await page.$('#continue-button');
+    const continuebutton = await page.$("#continue-button");
     await continuebutton.click();
     await page.waitForTimeout(100);
 
     // Check local storage
     const questiontype = await page.evaluate(() => {
-      return localStorage.getItem('questionType');
+      return localStorage.getItem("questionType");
     });
 
     expect(questiontype).toBe("relationship");
