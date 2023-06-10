5967c3fcef7c221638802b3d05d038dd11e20a91
Chengqianc <qiancheng0712@gmail.com>
2023-06-10 03:41:32 -0700

    Added Puppeteer single page tests for landing and response page, as well as unit tests for the function fetchResponses

--- source/tests/landingpuppeteer.test.js
+++ source/tests/landingpuppeteer.test.js
@@ -30,6 +30,7 @@ describe("Landing Page Tests", () => {
 
     expect(questiontype).toBe("health");
   });
+
   it("Click start, click relationships, click next, local storage should be relatiohsip", async () => {
     // Reset the page
     await page.evaluate(() => {
@@ -54,5 +55,57 @@ describe("Landing Page Tests", () => {
     });
 
     expect(questiontype).toBe("relationship");
+  });
+
+  it("Click start, click daily horoscope, click next, local storage should be daily", async () => {
+    // Reset the page
+    await page.evaluate(() => {
+      localStorage.clear();
+    });
+    await page.reload();
+
+    // Navigate through the landing page to get to skymap
+    const startbutton = await page.$("#start-button");
+    await startbutton.click();
+    await page.waitForTimeout(100);
+    const relationshipbutton = await page.$("#daily-horoscope-button");
+    await relationshipbutton.click();
+    await page.waitForTimeout(100);
+    const continuebutton = await page.$("#continue-button");
+    await continuebutton.click();
+    await page.waitForTimeout(100);
+
+    // Check local storage
+    const questiontype = await page.evaluate(() => {
+      return localStorage.getItem("questionType");
+    });
+
+    expect(questiontype).toBe("daily");
+  });
+
+  it("Click start, click career, click next, local storage should be career", async () => {
+    // Reset the page
+    await page.evaluate(() => {
+      localStorage.clear();
+    });
+    await page.reload();
+
+    // Navigate through the landing page to get to skymap
+    const startbutton = await page.$("#start-button");
+    await startbutton.click();
+    await page.waitForTimeout(100);
+    const relationshipbutton = await page.$("#career-button");
+    await relationshipbutton.click();
+    await page.waitForTimeout(100);
+    const continuebutton = await page.$("#continue-button");
+    await continuebutton.click();
+    await page.waitForTimeout(100);
+
+    // Check local storage
+    const questiontype = await page.evaluate(() => {
+      return localStorage.getItem("questionType");
+    });
+
+    expect(questiontype).toBe("career");
   });
 });
