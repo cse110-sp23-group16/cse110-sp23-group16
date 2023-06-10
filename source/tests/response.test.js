a084659bfaf632b9ae165ed11e20a1a80d39b394
Chengqianc <qiancheng0712@gmail.com>
2023-06-10 05:33:18 -0700

    Changed unit tests to focus on button functions

--- source/tests/response.test.js
+++ source/tests/response.test.js
@@ -32,20 +32,24 @@ describe("toggleText", () => {
     );
     expect(explanationClass).toBe(true);
 
-    // Assert that the button element is no longer hidden
-    const buttonClass = await page.$eval("#hiddenButton", (el) =>
-      el.classList.contains("hidden")
-    );
-    expect(buttonClass).toBe(false);
-
     // Assert that the next page button is visible
     const nextPageButtonClass = await page.$eval("#hiddenButton", (el) =>
       el.classList.contains("hidden")
     );
     expect(nextPageButtonClass).toBe(false);
+  });
 
-    // Assert that the text content is not empty
-    const textContent = await page.$eval("#hiddenText", (el) => el.textContent);
-    expect(textContent.trim() !== "").toBe(true);
+  it("should redirect to the thank you page when called", async () => {
+    // Call goToPage
+    await page.evaluate(() => {
+      goToPage();
+    });
+
+    // Wait for the redirect to happen
+    await page.waitForNavigation();
+
+    // Assert that the current URL is the thank you page URL
+    const currentURL = await page.url();
+    expect(currentURL).toContain("thankyou_page/thankyou.html");
   });
 });
